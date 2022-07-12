799136011c67902bbeb0c3fe96aa181e59faefbd
GitHub Action <action@github.com>
2022-07-12 13:07:22 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/motzkiste/zenscriptsupport/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.motzkiste.zenscriptsupport.services
+
+import com.github.motzkiste.zenscriptsupport.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
