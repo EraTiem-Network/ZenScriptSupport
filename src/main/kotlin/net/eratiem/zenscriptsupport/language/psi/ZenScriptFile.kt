e189f10b6370dd82f2ea0689f3c0d5ad5497dbb8
Motzkiste <motzkiste@proton.me>
2022-12-28 14:50:56 +0100

    added missing annotation

--- src/main/kotlin/net/eratiem/zenscriptsupport/language/psi/ZenScriptFile.kt
+++ src/main/kotlin/net/eratiem/zenscriptsupport/language/psi/ZenScriptFile.kt
@@ -4,8 +4,13 @@ import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.psi.FileViewProvider
 import net.eratiem.zenscriptsupport.language.ZenScriptFileType
 import net.eratiem.zenscriptsupport.language.ZenScriptLanguage
+import org.jetbrains.annotations.NotNull
 
-class ZenScriptFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, ZenScriptLanguage.INSTANCE) {
+class ZenScriptFile(
+    @NotNull viewProvider: FileViewProvider
+) : PsiFileBase(viewProvider, ZenScriptLanguage.INSTANCE) {
+
+    @NotNull
     override fun getFileType() = ZenScriptFileType.INSTANCE
 
     override fun toString(): String = "ZenScript-File"
