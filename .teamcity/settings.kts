d616964168de6a2e02bb7992b1d02a5f5135e0ca
TeamCity <TeamCity@TeamCity>
2023-09-15 17:31:08 +0000

    TeamCity change in 'EraTiem-Network / Plugins / ZenScriptSupport' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -38,6 +39,36 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Export Properties"
+            scriptContent = """
+                PROPERTIES="${'$'}(./gradlew properties --console=plain -q)" 
+                           VERSION="${'$'}(echo "${'$'}PROPERTIES" | grep "^version:" | cut -f2- -d ' ')" 
+                           NAME="${'$'}(echo "${'$'}PROPERTIES" | grep "^pluginName:" | cut -f2- -d ' ')" 
+                           CHANGELOG="${'$'}(./gradlew getChangelog --unreleased --no-header --console=plain -q)" 
+                           CHANGELOG="${'$'}{CHANGELOG//'%'/'%25'}" 
+                           CHANGELOG="${'$'}{CHANGELOG//${'$'}'\n'/'%0A'}" 
+                           CHANGELOG="${'$'}{CHANGELOG//${'$'}'\r'/'%0D'}" 
+                  
+                           echo "::set-output name=version::${'$'}VERSION" 
+                           echo "::set-output name=name::${'$'}NAME" 
+                           echo "::set-output name=changelog::${'$'}CHANGELOG" 
+                           echo "::set-output name=pluginVerifierHomeDir::~/.pluginVerifier" 
+                  
+                           ./gradlew listProductsReleases # prepare list of IDEs for Plugin Verifier
+            """.trimIndent()
+            param("org.jfrog.artifactory.selectedDeployableServer.publishBuildInfo", "true")
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "true")
+            param("org.jfrog.artifactory.selectedDeployableServer.buildDependencies", "Requires Artifactory Pro.")
+            param("org.jfrog.artifactory.selectedDeployableServer.urlId", "0")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.envVarsExcludePatterns", "*password*,*secret*")
+            param("org.jfrog.artifactory.selectedDeployableServer.targetRepo", "eratiem")
+        }
     }
 
     triggers {
